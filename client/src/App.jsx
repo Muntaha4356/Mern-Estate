de0ec01addf7be310a10a5dc105f089ffd17b984
Muntaha4356 <muntahapolaris@gmail.com>
2025-07-01 15:20:12 +0500

    Created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Profile from './Pages/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-         <h1 class="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-        </div>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+          <Route path='/signup'element={<Signup/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
