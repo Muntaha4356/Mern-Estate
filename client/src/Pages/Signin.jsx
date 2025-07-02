380c3c6d90a60f95d49df3550d6c1fca4aa1e2b3
Muntaha4356 <muntahapolaris@gmail.com>
2025-07-03 01:11:57 +0500

    Sign Up functionality done

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
 
 const Signin = () => {
-  return (
-    <div>
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData,  setFormData] = useState({
+    email:'',
+    password:'',
+  })
+  const handleChange = (e) =>{
+    setFormData((prev)=> ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const response = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        credentials: 'include',
+      });
+      const result = await response.json();
+      if(result.success){
+        alert("Logged in successfully!");
+        setError(null);
+        navigate('/');
+
+      }else {
+        alert(`Error: ${result.message}`);
+        setError(result.message)
       
+      }
+      setLoading(false);
+
+    }
+    catch (err) {
+      alert('Login failed. Please try again.');
+      setLoading(false);
+      setError(err);
+      console.error(err);}
+  }
+  return (
+      <div className='p-3 max-w-lg  mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4' onSubmit ={handleSubmit}>
+        
+        <input 
+        onChange={handleChange}
+        type='email' 
+        name='email'
+        value={formData.email}
+        placeholder='Email' 
+        className='border-none p-3 rounded-2xl'/>
+        <input 
+        onChange={handleChange}
+        type='password' 
+        name='password'
+        value={formData.password}
+        placeholder='Password' 
+        className='border-none p-3 rounded-2xl' />
+        <button 
+        type='submit' 
+        
+        className='bg-red-400 border-none p-3 rounded-2xl'>
+          {loading ? 'Loading...' : 'SignIn'}
+        </button>
+        <button 
+        className='border-none p-3 rounded-2xl bg-red-500'>
+          Continue with Google
+        </button>
+      </form>
+      <p className='p-3'>Don't Have an Account? <Link to={'/signup'}>Sign Up</Link></p>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -2,25 +2,88 @@ import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { useState } from 'react'
 const Signup = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false)
+  const [formData,  setFormData] = useState({
+      name:'',
+      email:'',
+      password:'',
+    })
+  const handleChange= (e) =>{
+    setFormData({
+      ...formData,
+      //whatever is changing change its value
+      [e.target.name] : e.target.value,
+    });
+    
+  }
+  console.log(formData);
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const response = await fetch('http://localhost:3000/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        credentials: 'include',
+      });
+      const result = await response.json();
+      if(result.success){
+        setLoading(false);
+        console.log('Sign up Successful')
+        setError(null);
+        navigate('/');
+      }else {
+        console.log(`Error: ${result.message}`);
+        setError(result.message)
+      }
+      setLoading(false);
+      
+
+    }
+    catch (err) {
+      setLoading(false);
+      console.log('Login failed. Please try again.');
+      console.error(err);
+      setError(err);
+    }
+  }
+  
   return (
     <div className='p-3 max-w-lg  mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit ={handleSubmit}>
         <input 
+        name='name'
+        value={formData.name}
+        onChange={handleChange}
         type='text' 
         placeholder='UseName' 
         className='border-none p-3 rounded-2xl'/>
         <input 
+        onChange={handleChange}
         type='email' 
+        name='email'
+        value={formData.email}
         placeholder='Email' 
         className='border-none p-3 rounded-2xl'/>
         <input 
+        onChange={handleChange}
         type='password' 
+        name='password'
+        value={formData.password}
         placeholder='Password' 
         className='border-none p-3 rounded-2xl' />
         <button 
+        type='submit' 
+        disabled={loading}
         className='bg-red-400 border-none p-3 rounded-2xl'>
-          Sign Up
+          {loading ? 'Loading...' : 'SignUp'}
         </button>
         <button 
         className='border-none p-3 rounded-2xl bg-red-500'>
@@ -28,7 +91,7 @@ const Signup = () => {
         </button>
       </form>
       <p className='p-3'>Have an Account? <Link to={'/signin'}>Sign in</Link></p>
-      
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
