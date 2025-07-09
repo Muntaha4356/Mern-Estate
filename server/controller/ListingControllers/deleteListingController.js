a393683c43e27ebf7ed837b6f5824386042b9dfe
Muntaha4356 <muntahapolaris@gmail.com>
2025-07-09 19:33:20 +0500

    Delete functionality working perfectly and everything else as welll...

--- server/controller/ListingControllers/deleteListingController.js
+++ server/controller/ListingControllers/deleteListingController.js
@@ -1,10 +1,25 @@
+import Listing from "../../models/listingModels.js";
+import {v2 as cloudinary} from 'cloudinary'
 export const deleteListController =async (req,res) => {
     //get the id of the list...
     const {listId} = req.body;
-    console.log(req.body);
     try{
+        
         if(!listId){
             return res.json({success: false, message: "ListId not provided to the server"})
+        }
+        const list = await Listing.findById(listId);
+        if (!list) {
+            return res.json({ success: false, message: "Listing not found" });
+        }
+        for (const image of list.imageUrls) {
+            if (image.public_id) {
+                try {
+                await cloudinary.uploader.destroy(image.public_id);
+                } catch (cloudErr) {
+                console.error(`Error deleting image from Cloudinary: ${image.public_id}`, cloudErr);
+                }
+            }
         }
         const deletedListing = await Listing.findByIdAndDelete(listId);
         if(!deletedListing){
