312fc0424340fd00f76fe87518f95dd4f8473afb
Muntaha <160359101+Muntaha4356@users.noreply.github.com>
2026-07-13 13:51:54 +0500

    fixed server js removed client dependency

--- server/server.js
+++ server/server.js
@@ -9,23 +9,17 @@ import dns from 'dns';
 import listRoutes from "./routes/listingRoutes.js";
 import path from 'path';
 
-
-
 const __dirname = path.resolve();
 const app = express();
-
-const port =process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 
 dns.setDefaultResultOrder('ipv4first');
 connectDb();
 
 app.use(express.json());
-
 app.use(cookieParser());
-//  ||'https://mern-estate-3x92430jn-muntaha4356s-projects.vercel.app' 
-const allowedOrigins = ['https://mern-estate-3x92430jn-muntaha4356s-projects.vercel.app'];
 
+const allowedOrigins = ['https://mern-estate-3x92430jn-muntaha4356s-projects.vercel.app'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -34,35 +28,25 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // if you are sending cookies or auth headers
+  credentials: true,
 }));
+
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRoutes);
-app.use('/api/list',listRoutes);
+app.use('/api/list', listRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('/', (req, res) => {
+  res.send('Server is Live');
+});
 
-
-
-//middleware to handle errors..not gonna use
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || 'Internal Server Error';
     return res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message
     });
 })
 
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });
-}
-
-app.listen(port , ()=> console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
