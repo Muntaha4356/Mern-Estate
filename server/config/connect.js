cfdb9c5e19df6a994fcc5d232a85afefdbeab89c
Muntaha <160359101+Muntaha4356@users.noreply.github.com>
2026-07-13 13:36:58 +0500

    mondodb file fixed

--- server/config/connect.js
+++ server/config/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb= async ()=>{
     mongoose.connection.on(`connected`, ()=>console.log("Database Connected"));
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/mern-estate`);
 
 }
 
